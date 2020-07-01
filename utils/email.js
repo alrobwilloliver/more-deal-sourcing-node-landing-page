4ba1eaaa892a28b68e291d8540aac8c694ad27ad
Alan Oliver <alrobwilloliver@gmail.com>
2020-07-01 02:53:21 +0100

    fixed errors and moved to google email host

--- utils/email.js
+++ utils/email.js
@@ -3,25 +3,29 @@ const nodemailer = require('nodemailer');
 const sendEmail = async options => {
     // Create a transporter
     const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        secure: true,
+        service: process.env.EMAIL_HOST,
         auth: {
             user: process.env.EMAIL_USERNAME,
             pass: process.env.EMAIL_PASSWORD
-        },
+        }
     })
 
     // Define the email options
     const mailOptions = {
-        from: 'Alan Oliver <alanoliver@gmail.com>',
+        from: 'Alan Oliver <moredealsourcing@gmail.com>',
         to: options.email,
         subject: options.subject,
         text: options.message,
         //html:
     }
     // Actually send the email with nodemailer
-    await transporter.sendMail(mailOptions);
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent: ' + info.response);
+        }
+    });
 }
 
 module.exports = sendEmail;
